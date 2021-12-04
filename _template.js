7fde7b8f45f063f20868f410d3105da61559510d
kuzznicki <kuznicki@protonmail.com>
2021-12-04 20:33:14 +0100

    day 4

---
+++ _template.js
@@ -0,0 +1,16 @@
+const input = getInput().split('\n');
+
+console.log('part one: ' + partOne(input)); // 
+console.log('part two: ' + partTwo(input)); // 
+
+function partOne(input) {
+    
+}
+
+function partTwo(input) {
+    
+}
+
+function getInput() {
+    return ``;
+}
